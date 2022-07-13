66d3322ba0be5c2a3749793f569048c17869735a
Morteza Jamshidi <devmor.j@gmail.com>
2022-07-13 20:59:52 +0430

    feat: detect swipes using custom vue directive

--- src/components/BaseCarousel.vue
+++ src/components/BaseCarousel.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { reactive, ref, onMounted, onUnmounted } from "vue";
+import type { Directive } from "vue";
 import useFullscreen from "@/composables/useFullscreen";
 
 /* ====================================================== */
@@ -196,6 +197,112 @@ function onDotClick(index: number): void {
     return;
   }
 }
+
+// onMounted(() => {});
+
+const vSwipe: Directive = {
+  mounted(swipeElement, binding) {
+    const startX = ref(0);
+    const startY = ref(0);
+    const startTime = ref(0);
+
+    const endX = ref(0);
+    const endY = ref(0);
+    const endTime = ref(0);
+
+    const xDiff = ref(0);
+    const yDiff = ref(0);
+    const timeDiff = ref(0);
+
+    type SwipeDirection = "left" | "right" | "up" | "down" | "none";
+
+    const swipeDirection = ref<SwipeDirection>("none");
+
+    // const swipeEvent = new CustomEvent('swipe', {
+    //   detail: {
+    //     dir: '',
+    //     duration: 0,
+    //   }
+    // })
+
+    swipeElement.addEventListener(
+      "touchstart",
+      (event: TouchEvent) => {
+        startTime.value = new Date().getTime();
+        startX.value = event.touches.item(0)?.clientX as number;
+        startY.value = event.touches.item(0)?.clientY as number;
+      },
+      { passive: true }
+    );
+
+    swipeElement.addEventListener(
+      "touchend",
+      (event: TouchEvent) => {
+        endTime.value = new Date().getTime();
+        timeDiff.value = endTime.value - startTime.value;
+
+        endX.value = event.changedTouches.item(0)?.clientX as number;
+        endY.value = event.changedTouches.item(0)?.clientY as number;
+
+        xDiff.value = endX.value - startX.value;
+        yDiff.value = endY.value - startY.value;
+
+        const getDirection = (xDiff: number, yDiff: number): SwipeDirection => {
+          if (Math.abs(xDiff) > Math.abs(yDiff)) {
+            if (xDiff >= 0) {
+              return "right";
+            } else return "left";
+          } else if (Math.abs(xDiff) < Math.abs(yDiff)) {
+            if (yDiff >= 0) {
+              return "down";
+            } else return "up";
+          } else {
+            // if program reaches here it means that:
+            // Math.abs(xDiff) === Math.abs(yDiff)
+            // so there is no direction
+            return "none";
+          }
+        };
+
+        swipeDirection.value = getDirection(xDiff.value, yDiff.value);
+
+        type CustomEventDetail = {
+          direction: SwipeDirection;
+          duration: number; // in ms
+        };
+
+        if (timeDiff.value > 50 && swipeDirection.value !== "none") {
+          binding.value(
+            new CustomEvent("swipe", {
+              detail: {
+                direction: swipeDirection.value,
+                duration: timeDiff.value,
+              } as CustomEventDetail,
+            })
+          );
+        }
+      },
+      { passive: true }
+    );
+  },
+};
+
+type SwipeDirection = "left" | "right" | "up" | "down";
+
+type CustomEventDetail = {
+  direction: SwipeDirection;
+  duration: number; // in ms
+};
+
+// [ ] use watch for xDiff and timeDiff
+// [ ] use swipeState with reactive instead of ref
+// [ ] create a useSwipe version
+
+function handleSwipe(event: CustomEvent<CustomEventDetail>) {
+  console.log(event.detail.direction);
+  if (event.detail.direction === "left") changeItem("next");
+  if (event.detail.direction === "right") changeItem("prev");
+}
 </script>
 
 <template>
@@ -207,7 +314,9 @@ function onDotClick(index: number): void {
     tabindex="-1"
     aria-label="carousel"
     role="region"
+    v-swipe="handleSwipe"
   >
+    <!-- @swipe="handleSwipe($event)" -->
     <slot :current="state.currentItem"></slot>
 
     <button
